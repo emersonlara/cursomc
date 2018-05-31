2db966b0ec11a3cc58f08644deb393411dba698b
Emerson Lara <emersonlara@gmail.com>
2018-05-31 13:22:43 -0400

    criando dominio

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,32 @@
 package com.nelioalves.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "O rest esta funcionando"; 
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1,"Categoria 1");
+		Categoria cat2 = new Categoria(2,"Categoria 2");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista; 
+		
+		
+		
 	}
 
 }
